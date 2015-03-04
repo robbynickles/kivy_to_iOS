292725a2e090f76454bc75abff5cd3dc9c74889b
Robby Nickles <r.nickles7@gmail.com>
2015-03-04 12:38:47 -0800

    Updated the script's comments.

--- kivyToApp3.sh
+++ kivyToApp3.sh
@@ -10,22 +10,28 @@ fi
 PROJECT=$(basename $PROJECT_DIR)
 PROJECT=$(echo $PROJECT | tr '[A-Z]' '[a-z]')
 
-# Create XCode project from kivy project.
+
+
+# 1) Create XCode project from kivy project.
+
 sudo rm -R /Users/robby/Desktop/apps/kivy-ios/app-$PROJECT
 ~/Desktop/apps/kivy-ios/tools/create-xcode-project.sh $PROJECT $PROJECT_DIR
 echo "Building application for iPhone into ~/Desktop/apps/kivy-ios/app-$PROJECT/build/Release-iphoneos/"
 echo 
 xcodebuild -project /Users/robby/Desktop/apps/kivy-ios/app-$PROJECT/$PROJECT.xcodeproj -configuration Release > /dev/null
 
-# Copy .app directory to the build dir.
-# Run ldid on the executable.
+
+# 2) Run ldid on the executable.
+
 APP_DIR=$(find /Users/robby/Desktop/apps/kivy-ios/app-$PROJECT/build/Release-iphoneos -name $PROJECT'.app')
 BUILD_DIR=~/Desktop/apps
 echo "Running ldid on the executable $APP_DIR/$PROJECT"
 echo 
 $BUILD_DIR/ldid -S $APP_DIR/$PROJECT
 
-# Install the .app to the phone's /Applications via ssh via usb.
+
+# 3) Install the app to the phone's /Applications via ssh via usb.
+
 echo "Killing any instances of the executable named '$PROJECT' currently running on the phone"
 ssh -p 2222 root@localhost "killall $PROJECT; rm -rf /Applications/$PROJECT.app"
 echo
